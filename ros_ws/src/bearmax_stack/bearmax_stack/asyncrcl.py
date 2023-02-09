86476fba0efc44c8bc922070a1ddb208e41d350a
Simon Weizman <contact@simon.weizman.us>
2023-02-08 19:34:36 -0500

    laid groundwork for stack connector node

---
+++ ros_ws/src/bearmax_stack/bearmax_stack/asyncrcl.py
@@ -0,0 +1,24 @@
+import threading
+import asyncio
+import rclpy
+from rclpy.node import Node
+
+async def spin(node: Node):
+    cancel = node.create_guard_condition(lambda: None)
+    def _spin(node: Node,
+              future: asyncio.Future,
+              event_loop: asyncio.AbstractEventLoop):
+        while not future.cancelled():
+            rclpy.spin_once(node)
+        if not future.cancelled():
+            event_loop.call_soon_threadsafe(future.set_result, None)
+    event_loop = asyncio.get_event_loop()
+    spin_task = event_loop.create_future()
+    spin_thread = threading.Thread(target=_spin, args=(node, spin_task, event_loop))
+    spin_thread.start()
+    try:
+        await spin_task
+    except asyncio.CancelledError:
+        cancel.trigger()
+    spin_thread.join()
+    node.destroy_guard_condition(cancel)
--- ros_ws/src/bearmax_stack/bearmax_stack/stack_connector.py
+++ ros_ws/src/bearmax_stack/bearmax_stack/stack_connector.py
@@ -3,27 +3,87 @@ from rclpy.node import Node
 
 from bearmax_msgs import StackCommand
 
-DEFAULT_WS_URL = 'ws://localhost'
+import asyncio
+import socketio
+
+from asyncrcl import spin
+
+DEFAULT_WS_URL = 'http://localhost:8080'
 
 class StackConnector(Node):
-    def __init__(self):
+    sio_ = socketio.AsyncClient()
+
+    def __init__(self, sio: socketio.AsyncClient):
         super().__init__('stack_connector')
 
-        self.declare_parameter('ws_url', )
+        self.sio_ = sio
+        
+        self.publisher_ = self.create_publisher(StackCommand, 'stackcommand', 10)
 
-    def timer_callback(self):
-        self.get_logger().info('Test Log Entry')
+        self.declare_parameter('ws_url', DEFAULT_WS_URL)
 
-def main(args=None):
-    rclpy.init(args=args)
+        self.register_handlers()
 
-    stack_connector = StackConnector()
+        asyncio.get_event_loop().create_task(
+            self.sio_.connect(self.wsl_url)
+        )
 
-    rclpy.spin(stack_connector)
+    @property
+    def wsl_url(self) -> str:
+        return self.get_parameter('wsl_url').get_parameter_value().string_value
+
+    async def publish_cmd(self, message: StackCommand):
+        self.publisher_.publish(message)
+
+    def register_handlers(self):
+        @self.sio_.event
+        async def connect():
+            self.get_logger().info('Connected to WebSocket Server')
+
+        @self.sio_.event
+        async def connect_error(data):
+            self.get_logger().error(
+                f"Error trying to connect to WebSocket Server: {data}",
+            )
+
+        @self.sio_.event
+        async def disconnect():
+            self.get_logger().info('Disconnected from WebSocket Server')
+
+        @self.sio_.event
+        async def command(data):
+            self.get_logger().info(f"Recieved Command: {data}")
+
+            await self.publish_cmd(data)
+
+async def main():
+
+    sio = socketio.AsyncClient()
+
+    stack_connector = StackConnector(sio)
+
+    # create tasks for spinning and sleeping
+    spin_task = asyncio.get_event_loop().create_task(spin(stack_connector))
+    sleep_task = asyncio.get_event_loop().create_task(asyncio.sleep(5.0))
+
+    # concurrently execute both tasks
+    await asyncio.wait([spin_task, sleep_task], return_when=asyncio.FIRST_COMPLETED)
+
+    # cancel tasks
+    if spin_task.cancel():
+        await spin_task
+    if sleep_task.cancel():
+        await sleep_task
+
+    if (sio.connected):
+        await sio.disconnect()
 
     stack_connector.destroy_node()
 
     rclpy.shutdown()
 
 if __name__ == '__main__':
-    main()
+    rclpy.init()
+    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.get_event_loop().close()
+    rclpy.shutdown()
