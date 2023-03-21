4a982e84c3bb456110ee97cb902f0dedd2a46e0f
Simon Weizman <contact@simon.weizman.us>
2023-03-20 21:17:35 -0400

    Create camera node launch file

---
+++ ros_ws/src/bearmax_emotion/launch/camera.launch.py
@@ -0,0 +1,17 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    camera = Node(
+        package="v4l2_camera",
+        executable="v4l2_camera_node",
+        output="screen",
+        parameters=[{
+            "image_size": [640, 480],
+            "camera_frame_id": "camera_optical_link"
+        }]
+    )
+
+    return LaunchDescription([camera])
