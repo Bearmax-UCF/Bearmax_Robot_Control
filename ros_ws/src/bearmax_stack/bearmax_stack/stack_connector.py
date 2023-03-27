6b770f9f7e851eced3d91d907c542c24680eec14
Simon Weizman <contact@simon.weizman.us>
2023-03-27 15:59:32 -0400

    stack_connector events

--- ros_ws/src/bearmax_stack/bearmax_stack/stack_connector.py
+++ ros_ws/src/bearmax_stack/bearmax_stack/stack_connector.py
@@ -14,31 +14,35 @@ from .asyncrcl import spin
 
 DEFAULT_WS_URL = 'https://localhost:8080'
 
+
 class StackConnector(Node):
     def __init__(self, sio: socketio.AsyncClient):
         super().__init__('stack_connector')
 
         self.sio_ = sio
-        
-        self.publisher_ = self.create_publisher(StackCommand, 'stackcommand', 10)
 
-        self.subscriber_ = self.create_subscription(String, "to_stack", self.send_to_stack, 10)
+        self.publisher_ = self.create_publisher(
+            String, 'stackcommand', 10)
+
+        self.subscriber_ = self.create_subscription(
+            StackCommand, "to_stack", self.send_to_stack, 10)
 
         self._ws_url = self.declare_parameter('ws_url', DEFAULT_WS_URL)
 
-        self.get_logger().info(self._ws_url.value)
+        self.log_info(self._ws_url.value)
 
         self.register_handlers()
 
         self._event_loop = asyncio.get_running_loop()
-        
+
         self._event_loop.create_task(
             self.connect()
         )
 
-    async def send_to_stack(self, msg):
-        self.get_logger().info(f"[Sent to Stack]: {msg.data}")
-        await self.sio_.send(msg.data)
+    async def send_to_stack(self, request):
+        self.log_info(
+            f"[Sent to Stack]: {{event: {request.event}, data: {request.data}}}")
+        await self.sio_.emit(request.event, request.data)
 
     async def connect(self):
         await self.sio_.connect(self.wsl_url)
@@ -48,33 +52,46 @@ class StackConnector(Node):
     def wsl_url(self) -> str:
         return self._ws_url.get_parameter_value().string_value
 
-    async def publish_cmd(self, message: StackCommand):
-        self.publisher_.publish(message)
-
     def register_handlers(self):
         @self.sio_.event
         async def connect():
-            self.get_logger().info('Connected to WebSocket Server')
+            self.log_info('Connected to WebSocket Server')
 
         @self.sio_.event
         async def connect_error(data):
-            self.get_logger().error(
+            self.log_err(
                 f"Error trying to connect to WebSocket Server: {data}",
             )
 
         @self.sio_.event
         async def disconnect():
-            self.get_logger().info('Disconnected from WebSocket Server')
+            self.log_info('Disconnected from WebSocket Server')
 
         @self.sio_.event
         async def disconnecting():
-            self.get_logger().info('Disconnecting from WebSocket Server')
+            self.log_info('Disconnecting from WebSocket Server')
 
         @self.sio_.event
-        async def command(data):
-            self.get_logger().info(f"Recieved Command: {data}")
+        async def emotionGame(action):
+            if not action in ("start", "stop"):
+                self.log_err(
+                    f"emotionGame handler expected string action and got {action}")
+            # "emotionStart" or "emotionStop"
+            self.publisher_.publish("emotion" + action.capitalize())
 
-            await self.publish_cmd(data)
+        @self.sio_.event
+        async def recalibrate():
+            self.publisher_.publish("recalibrate")
+
+        @self.sio_.event
+        async def ping():
+            self.log_info("Pong!")
+
+    def log_info(self, msg):
+        self.get_logger().info(msg)
+
+    def log_err(self, msg):
+        self.get_logger().error(msg)
 
 
 async def run(args=None):
@@ -85,10 +102,10 @@ async def run(args=None):
         ssl_context.load_verify_locations(SCERT_PATH)
     with path("bearmax_stack.ssl", "client-crt.pem") as CCERT_PATH, path("bearmax_stack.ssl", "client-key.pem") as CKEY_PATH:
         ssl_context.load_cert_chain(certfile=CCERT_PATH, keyfile=CKEY_PATH)
-        
+
     connector = aiohttp.TCPConnector(ssl=ssl_context)
     async with aiohttp.ClientSession(connector=connector) as http_session:
-        sio = socketio.AsyncClient(http_session=http_session)
+        sio = socketio.AsyncClient(http_session=http_session, logger=True)
 
         stack_connector = StackConnector(sio)
 
@@ -106,12 +123,21 @@ async def run(args=None):
         stack_connector.destroy_node()
     rclpy.shutdown()
 
+
+def createStackCommand(event, data) -> StackCommand:
+    msg = StackCommand()
+    msg.event = event
+    msg.data = data
+    return msg
+
+
 def main(args=None):
     try:
         asyncio.run(run(args=args))
     except RuntimeError:
         # FIXME: There must be a better way to handle cleanup of async stuff
         pass
+
 
 if __name__ == "__main__":
     main()
