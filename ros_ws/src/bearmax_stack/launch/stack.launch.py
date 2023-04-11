bb3bf197a54e556d2a24f28be21919f510e6f2da
Simon Weizman <contact@simon.weizman.us>
2023-04-11 19:04:47 -0400

    Fixes

--- ros_ws/src/bearmax_stack/launch/stack.launch.py
+++ ros_ws/src/bearmax_stack/launch/stack.launch.py
@@ -6,22 +6,21 @@ from launch.actions import DeclareLaunchArgument
 def generate_launch_description():
     ws_url = LaunchConfiguration("ws_url")
 
-        stack = Node(
-            package="bearmax_stack",
-            executable="stack_connector",
-            parameters=[
-                {"ws_url": ws_url}
-                    ]
-                )
+    stack = Node(
+        package="bearmax_stack",
+        executable="stack_connector",
+        parameters=[
+            {"ws_url": ws_url}
+        ]
+    )
 
-            return LaunchDescription(
-            [
-                DeclareLaunchArgument(
-                    "ws_url",
-                    default_value="https://carewithbearmax.com",
-                    description="The base url of the stack."
-                    ),
-                stack
-                ]
-                )
-
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "ws_url",
+                default_value="https://carewithbearmax.com",
+                description="The base url of the stack."
+            ),
+            stack
+        ]
+    )
